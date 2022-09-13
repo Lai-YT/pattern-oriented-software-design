277909ef0c341ff424caf48ed2458f335d4cfefb
Lai-YT <t108820026@ntut.org.tw>
2022-09-13 08:09:09 +0800

    feat: equality & inequality operator

--- src/point.h
+++ src/point.h
@@ -9,12 +9,23 @@ class Point {
 
   double y() const { return y_; }
 
+  /*
+   * Returns the point in string "(x, y)".
+   *
+   * Floating-point numbers are rounded to the second decimal places.
+   */
   std::string info() const {
     auto ss = std::stringstream{};
     ss.precision(3);
     ss << '(' << x_ << ", " << y_ << ')';
     return ss.str();
   }
+
+  bool operator==(const Point& that) const {
+    return (&that == this) || (that.x_ == this->x_ && that.y_ == this->y_);
+  }
+
+  bool operator!=(const Point& that) const { return !operator==(that); }
 
  private:
   double x_;
--- test/ut_point.h
+++ test/ut_point.h
@@ -14,15 +14,51 @@ TEST(PointTest, CheckXYSetProperly) {
 TEST(PointTest, TestInfoOnIntegerXY) {
   const auto p = Point{2, -3};
 
-  const std::string info = p.info();
-
-  ASSERT_EQ("(2, -3)", info);
+  ASSERT_EQ("(2, -3)", p.info());
 }
 
 TEST(PointTest, TestInfoOnDoubleXYShouldRoundToTwoDecimalPlaces) {
   const auto p = Point{-4.586, -3.471};
 
-  const std::string info = p.info();
+  ASSERT_EQ("(-4.59, -3.47)", p.info());
+}
 
-  ASSERT_EQ("(-4.59, -3.47)", info);
+TEST(PointTest, TestEqualityOnSameXY) {
+  const auto a = Point{2, -3};
+  const auto b = Point{2, -3};
+
+  ASSERT_TRUE(a == b);
+}
+
+TEST(PointTest, TestEqualityOnSelfComparison) {
+  const auto p = Point{2, -3};
+
+  ASSERT_TRUE(p == p);
+}
+
+TEST(PointTest, TestEqualityOnDifferentXY) {
+  const auto a = Point{2, -3};
+  const auto b = Point{-3, 2};
+
+  ASSERT_FALSE(a == b);
+}
+
+TEST(PointTest, TestInequalityOnDifferentXY) {
+  const auto a = Point{2, -3};
+  const auto b = Point{-3, 2};
+
+  ASSERT_TRUE(a != b);
+}
+
+TEST(PointTest, TestInequalityOnSameXY) {
+  const auto a = Point{2, -3};
+  const auto b = Point{2, -3};
+
+  ASSERT_FALSE(a != b);
+}
+
+TEST(PointTest, TestInequalityOnSelfComparison) {
+  const auto p = Point{2, -3};
+
+  ASSERT_FALSE(p != p);
 }
