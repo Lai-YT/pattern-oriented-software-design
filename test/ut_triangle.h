a82abb242adf3ef18e804d4d59f74832cf553ca2
Lai-YT <t108820026@ntut.org.tw>
2022-09-14 13:37:32 +0800

    feat: adapt triangle

--- test/ut_triangle.h
+++ test/ut_triangle.h
@@ -1,25 +1,32 @@
 #include <gtest/gtest.h>
 
+#include "../src/point.h"
 #include "../src/triangle.h"
 #include "../src/two_dimensional_vector.h"
 
 TEST(TriangleTest, TestPerimeter) {
-  const auto tri = Triangle{TwoDimensionalVector{{0, 0}, {3, 0}},
-                            TwoDimensionalVector{{3, 4}, {3, 0}}};
+  const auto tri =
+      Triangle{new TwoDimensionalVector{new Point{0, 0}, new Point{3, 0}},
+               new TwoDimensionalVector{new Point{3, 4}, new Point{3, 0}}};
 
   ASSERT_NEAR(12, tri.perimeter(), 0.001);
 }
 
 TEST(TriangleTest, TestArea) {
-  const auto tri = Triangle{TwoDimensionalVector{{0, 0}, {3, 0}},
-                            TwoDimensionalVector{{3, 4}, {3, 0}}};
+  const auto tri =
+      Triangle{new TwoDimensionalVector{new Point{0, 0}, new Point{3, 0}},
+               new TwoDimensionalVector{new Point{3, 4}, new Point{3, 0}}};
 
   ASSERT_NEAR(6, tri.area(), 0.001);
 }
 
 TEST(TriangleTest, TestInfo) {
-  const auto tri = Triangle{TwoDimensionalVector{{0, 0}, {3, 0}},
-                            TwoDimensionalVector{{3, 4}, {3, 0}}};
+  const auto tri =
+      Triangle{new TwoDimensionalVector{new Point{0, 0}, new Point{3, 0}},
+               new TwoDimensionalVector{new Point{3, 4}, new Point{3, 0}}};
 
-  ASSERT_EQ("Triangle (Vector ((0.00, 0.00), (3.00, 0.00)), Vector ((3.00, 4.00), (3.00, 0.00)))", tri.info());
+  ASSERT_EQ(
+      "Triangle (Vector ((0.00, 0.00), (3.00, 0.00)), Vector ((3.00, 4.00), "
+      "(3.00, 0.00)))",
+      tri.info());
 }
