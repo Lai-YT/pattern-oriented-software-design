78b8bf68a660d46ed3eb3c2004b5c3a39ff5cf5c
Lai-YT <381xvmvbib@gmail.com>
2022-11-25 04:21:44 +0000

    test: instances should be identical under multi-threading

--- test/iterator/factory/ut_iterator_factory.h
+++ test/iterator/factory/ut_iterator_factory.h
@@ -1,7 +1,11 @@
 #include <gtest/gtest.h>
 
+#include <array>
+#include <chrono>
 #include <string>
+#include <thread>
 #include <type_traits>
+#include <unordered_set>
 
 #include "../../../src/iterator/factory/bfs_iterator_factory.h"
 #include "../../../src/iterator/factory/dfs_iterator_factory.h"
@@ -49,10 +53,29 @@ TEST(IteratorFactoryTest, TestCopyConstructorShouldNotBePublic) {
   EXPECT_FALSE(std::is_copy_constructible<ListIteratorFactory>::value);
 }
 
-
 TEST(IteratorFactoryTest, TestCopyAssignmentShouldNotBePublic) {
   EXPECT_FALSE(std::is_copy_assignable<IteratorFactory>::value);
   EXPECT_FALSE(std::is_copy_assignable<BFSIteratorFactory>::value);
   EXPECT_FALSE(std::is_copy_assignable<DFSIteratorFactory>::value);
   EXPECT_FALSE(std::is_copy_assignable<ListIteratorFactory>::value);
+}
+
+/* This test is unstable. */
+TEST(IteratorFactoryTest,
+     TestGetInstanceListShouldBeIdenticalUnderMultiThreading) {
+  auto threads = std::array<std::thread, 1000>{};
+  auto factories = std::unordered_set<IteratorFactory*>{};
+  const std::string type = "BFS";
+
+  for (auto&& thread : threads) {
+    thread = std::thread([&type, &factories]() -> void {
+      std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+      factories.insert(IteratorFactory::getInstance(type));
+    });
+  }
+  for (auto&& thread : threads) {
+    thread.join();
+  }
+
+  ASSERT_EQ(1, factories.size());
 }
