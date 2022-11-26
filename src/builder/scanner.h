e42a3d2bda3e66799053239834efdc806d121947
Lai-YT <381xvmvbib@gmail.com>
2022-11-26 05:14:44 +0000

    refactor: extract function

--- src/builder/scanner.h
+++ src/builder/scanner.h
@@ -11,39 +11,43 @@ class Scanner {
   Scanner(const std::string& input) : input_{input} {}
 
   std::string next() {
-    auto word = std::string{};
     SkipWhiteSpace_();
+
+    auto word = std::string{};
     do {
       word.clear();
       char c = input_.at(pos_);
-      if (IsToken_(std::string{c})) {
+      /* single-character token */
+      if (IsToken_(c)) {
         word = c;
         ++pos_;
         break;
       }
-      while (!isDone() && !IsWhiteSpace_(c) && !IsToken_(std::string{c}) &&
-             !IsToken_(word)) {
+      /* normal-string token */
+      while (!isDone() && !IsWhiteSpace_(input_.at(pos_)) &&
+             !IsToken_(input_.at(pos_)) && !IsToken_(word)) {
         word += input_.at(pos_);
-        c = input_.at(++pos_);
-        std::cout << word + '\n';
+        ++pos_;
       }
     } while (!IsToken_(word) && !isDone());
+
     SkipWhiteSpace_();
     return IsToken_(word) ? word : "";
   }
 
   double nextDouble() {
     SkipWhiteSpace_();
+
     std::string double_ = "";
-    while (!std::isdigit(input_.at(pos_)) && input_.at(pos_) != '+' &&
-           input_.at(pos_) != '-') {
+    while (!std::isdigit(input_.at(pos_)) && !IsSign_(input_.at(pos_))) {
       ++pos_;
     }
-    if (input_.at(pos_) == '+' || input_.at(pos_) == '-') {
+    if (IsSign_(input_.at(pos_))) {
       double_ += input_.at(pos_++);
     }
-    std::cout << double_ + '\n';
     if (std::isdigit(input_.at(pos_))) {
+      /* a floating-point number has 3 parts:
+       * integer part, decimal point and fractional part */
       while (std::isdigit(input_.at(pos_))) {
         double_ += input_.at(pos_++);
       }
@@ -54,7 +58,7 @@ class Scanner {
         double_ += input_.at(pos_++);
       }
     }
-    std::cout << double_ + '\n';
+
     SkipWhiteSpace_();
     return std::stod(double_);
   }
@@ -79,8 +83,16 @@ class Scanner {
     return c == ' ' || c == '\n';
   }
 
+  bool IsSign_(const char c) const {
+    return c == '+' || c == '-';
+  }
+
   bool IsToken_(const std::string& candidate) const {
     return TOKENS_.find(candidate) != TOKENS_.cend();
+  }
+
+  bool IsToken_(const char c) const {
+    return IsToken_(std::string{c});
   }
 };
 
