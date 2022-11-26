4a8070388719fdf47d4832af6bba3dfeb3a48427
Lai-YT <381xvmvbib@gmail.com>
2022-11-26 07:22:51 +0000

    refactor: extract function & return array to show size

--- src/builder/shape_parser.h
+++ src/builder/shape_parser.h
@@ -28,62 +28,14 @@ class ShapeParser {
     while (!scanner_.isDone()) {
       auto token = std::string{scanner_.next()};
       if (token == "Circle") {
-        auto* center = new Point{scanner_.nextDouble(), scanner_.nextDouble()};
-        auto* on_circle =
-            new Point{scanner_.nextDouble(), scanner_.nextDouble()};
-        DeleteLater_(center);
-        DeleteLater_(on_circle);
-        builder_.buildCircle(center, on_circle);
-        SkipTripleCloseParenthesis_();
+        ParseCircle_();
       } else if (token == "Triangle") {
-        auto vertices = std::set<Point, std::function<bool(Point, Point)>>{
-            {
-                Point{scanner_.nextDouble(), scanner_.nextDouble()},
-                Point{scanner_.nextDouble(), scanner_.nextDouble()},
-                Point{scanner_.nextDouble(), scanner_.nextDouble()},
-                Point{scanner_.nextDouble(), scanner_.nextDouble()},
-            },
-            [](const Point& p1, const Point& p2) -> bool {
-              return p1.info() < p2.info();
-            }};
-        auto pointers_to_vertex = std::vector<const Point*>{};
-        for (auto&& p : vertices) {
-          pointers_to_vertex.push_back(new Point{p});
-        }
-        assert(pointers_to_vertex.size() == 3);
-        for (const Point* p : pointers_to_vertex) {
-          DeleteLater_(p);
-        }
-        builder_.buildTriangle(pointers_to_vertex.at(0),
-                               pointers_to_vertex.at(1),
-                               pointers_to_vertex.at(2));
-        SkipTripleCloseParenthesis_();
+        ParseTriangle_();
       } else if (token == "Rectangle") {
-        auto vertices = std::set<Point, std::function<bool(Point, Point)>>{
-            {
-                Point{scanner_.nextDouble(), scanner_.nextDouble()},
-                Point{scanner_.nextDouble(), scanner_.nextDouble()},
-                Point{scanner_.nextDouble(), scanner_.nextDouble()},
-                Point{scanner_.nextDouble(), scanner_.nextDouble()},
-            },
-            [](const Point& p1, const Point& p2) -> bool {
-              return p1.info() < p2.info();
-            }};
-        auto pointers_to_vertex = std::vector<const Point*>{};
-        for (auto&& p : vertices) {
-          pointers_to_vertex.push_back(new Point{p});
-        }
-        assert(pointers_to_vertex.size() == 3);
-        for (const Point* p : pointers_to_vertex) {
-          DeleteLater_(p);
-        }
-        builder_.buildRectangle(pointers_to_vertex.at(0),
-                                pointers_to_vertex.at(1),
-                                pointers_to_vertex.at(2));
-        SkipTripleCloseParenthesis_();
+        ParseRectangle_();
       } else if (token == "CompoundShape") {
         builder_.buildCompoundShape();
-        scanner_.next(); /* ( */
+        scanner_.next(); /* skip open parenthesis */
       } else if (token == ")") {
         builder_.buildCompoundEnd();
       }
@@ -101,6 +53,60 @@ class ShapeParser {
   /* This is to extend the life time of the points,
    * but they are still deleted with the parser. */
   std::vector<const Point*> points_to_delete_{};
+
+  void ParseCircle_() {
+    auto* center = new Point{scanner_.nextDouble(), scanner_.nextDouble()};
+    auto* on_circle = new Point{scanner_.nextDouble(), scanner_.nextDouble()};
+    DeleteLater_(center);
+    DeleteLater_(on_circle);
+    builder_.buildCircle(center, on_circle);
+    SkipTripleCloseParenthesis_();
+  }
+
+  void ParseTriangle_() {
+    std::array<const Point*, 3> vertices =
+        ExtractThreeUnDuplicatedVerticesFromNextFourPoints_();
+    builder_.buildTriangle(vertices.at(0), vertices.at(1), vertices.at(2));
+    SkipTripleCloseParenthesis_();
+  }
+
+  void ParseRectangle_() {
+    std::array<const Point*, 3> vertices =
+        ExtractThreeUnDuplicatedVerticesFromNextFourPoints_();
+    builder_.buildRectangle(vertices.at(0), vertices.at(1), vertices.at(2));
+    SkipTripleCloseParenthesis_();
+  }
+
+  /* The 3 un-duplicated vertices will be deleted later. */
+  std::array<const Point*, 3>
+  ExtractThreeUnDuplicatedVerticesFromNextFourPoints_() {
+    auto points_info_less_than = [](const Point& p1, const Point& p2) -> bool {
+      return p1.info() < p2.info();
+    };
+    /* Since two of them are common points, put them into a set to eliminate the
+     * common point. */
+    auto vertices = std::set<Point, decltype(points_info_less_than)>{
+        {
+            Point{scanner_.nextDouble(), scanner_.nextDouble()},
+            Point{scanner_.nextDouble(), scanner_.nextDouble()},
+            Point{scanner_.nextDouble(), scanner_.nextDouble()},
+            Point{scanner_.nextDouble(), scanner_.nextDouble()},
+        },
+        points_info_less_than};
+    assert(vertices.size() == 3);
+
+    /* Allocate them on the heap so can extend their life-time. */
+    auto pointers_of_vertices = std::array<const Point*, 3>{};
+    int i = 0;
+    for (auto&& p : vertices) {
+      pointers_of_vertices.at(i) = new Point{p};
+      ++i;
+    }
+    for (const Point* p : pointers_of_vertices) {
+      DeleteLater_(p);
+    }
+    return pointers_of_vertices;
+  }
 
   void DeleteLater_(const Point* const to_delete) {
     points_to_delete_.push_back(to_delete);
