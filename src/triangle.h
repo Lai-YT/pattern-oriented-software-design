ba693a5b59303d3cbcbaefe0bc3844f5c6e040bd
Lai-YT <381xvmvbib@gmail.com>
2022-12-06 03:17:01 +0000

    feat: Triangle supports pass by reference

--- src/triangle.h
+++ src/triangle.h
@@ -17,29 +17,36 @@ class Triangle : public Shape {
  public:
   Triangle(const TwoDimensionalVector* const side_1,
            const TwoDimensionalVector* const side_2)
-      : side_1_{side_1}, side_2_{side_2} {
-    if (side_1_->cross(side_2_) == 0) {
+      : Triangle{*side_1, *side_2} {}
+
+  Triangle(const TwoDimensionalVector& side_1,
+           const TwoDimensionalVector& side_2)
+      : side_1_{side_1},
+        side_2_{side_2},
+        side_3_{{0, 0},
+                {0, 0}} /* dummy value since Point has no default ctor */ {
+    if (side_1_.cross(side_2_) == 0) {
       throw ParallelSideException{"sides should not be parallel"};
     }
-    const Point* common_point = FindCommonPointOfVectors(*side_1, *side_2_);
+    const Point* common_point = FindCommonPointOfVectors(side_1, side_2_);
     if (common_point == nullptr) {
       throw NoCommonPointException{"sides should intersect at one end"};
     }
-    side_3_ = std::unique_ptr<TwoDimensionalVector>{new TwoDimensionalVector{
-        FindUncommonPointFromVector(*side_1_, *common_point),
-        FindUncommonPointFromVector(*side_2_, *common_point)}};
+    side_3_ = TwoDimensionalVector{
+        FindUncommonPointFromVector(side_1_, *common_point),
+        FindUncommonPointFromVector(side_2_, *common_point)};
   }
 
   double perimeter() const override {
-    return side_1_->length() + side_2_->length() + side_3_->length();
+    return side_1_.length() + side_2_.length() + side_3_.length();
   }
 
   double area() const override {
-    return std::abs(side_1_->cross(side_2_)) / 2;
+    return std::abs(side_1_.cross(side_2_)) / 2;
   }
 
   std::string info() const override {
-    return "Triangle (" + side_1_->info() + ", " + side_2_->info() + ")";
+    return "Triangle (" + side_1_.info() + ", " + side_2_.info() + ")";
   }
 
   /**
@@ -50,10 +57,10 @@ class Triangle : public Shape {
    */
   std::set<const Point*> getPoints() const override {
     const Point common_between_side_1_and_2 =
-        *FindCommonPointOfVectors(*side_1_, *side_2_);
+        *FindCommonPointOfVectors(side_1_, side_2_);
     const Point uncommon_in_side_2_with_side_1 =
-        *FindUncommonPointFromVector(*side_2_, common_between_side_1_and_2);
-    return {new Point{side_1_->head()}, new Point{side_1_->tail()},
+        *FindUncommonPointFromVector(side_2_, common_between_side_1_and_2);
+    return {new Point{side_1_.head()}, new Point{side_1_.tail()},
             new Point{uncommon_in_side_2_with_side_1}};
   }
 
@@ -74,9 +81,9 @@ class Triangle : public Shape {
   };
 
  private:
-  const TwoDimensionalVector* side_1_;
-  const TwoDimensionalVector* side_2_;
-  std::unique_ptr<const TwoDimensionalVector> side_3_;
+  TwoDimensionalVector side_1_;
+  TwoDimensionalVector side_2_;
+  TwoDimensionalVector side_3_;
 };
 
 #endif /* end of include guard: SRC_TRIANGLE_H_ */
--- test/ut_triangle.h
+++ test/ut_triangle.h
@@ -11,18 +11,10 @@
 #include "../src/two_dimensional_vector.h"
 
 class TriangleTest : public ::testing::Test {
- private:
-  const Point vector_head_1_{0, 0};
-  const Point vector_tail_1_{3, 0};
-  const TwoDimensionalVector vector_1_{&vector_head_1_, &vector_tail_1_};
-  const Point vector_head_2_{3, 4};
-  const Point vector_tail_2_{3, 0};
-  const TwoDimensionalVector vector_2_{&vector_head_2_, &vector_tail_2_};
-
  protected:
   const double DELTA = 0.001;
 
-  Triangle triangle_{&vector_1_, &vector_2_};
+  Triangle triangle_{{{0, 0}, {3, 0}}, {{3, 4}, {3, 0}}};
 };
 
 TEST_F(TriangleTest, TestPerimeter) {
@@ -41,24 +33,16 @@ TEST_F(TriangleTest, TestInfo) {
 }
 
 TEST_F(TriangleTest, PassingVectorsWithNoCommonPointShouldThrowException) {
-  const auto vector_head_1 = Point{0, 0};
-  const auto vector_tail_1 = Point{4, 0};
-  const auto vector_1 = TwoDimensionalVector{&vector_head_1, &vector_tail_1};
-  const auto vector_head_2 = Point{3, 4};
-  const auto vector_tail_2 = Point{3, 0};
-  const auto vector_2 = TwoDimensionalVector{&vector_head_2, &vector_tail_2};
+  const auto vector_1 = TwoDimensionalVector{{0, 0}, {4, 0}};
+  const auto vector_2 = TwoDimensionalVector{{3, 4}, {3, 0}};
 
   ASSERT_THROW({ const auto triangle = Triangle(&vector_1, &vector_2); },
                Triangle::NoCommonPointException);
 }
 
 TEST_F(TriangleTest, PassingParallelVectorsShouldThrowException) {
-  const auto vector_head_1 = Point{0, 0};
-  const auto vector_tail_1 = Point{4, 0};
-  const auto vector_1 = TwoDimensionalVector{&vector_head_1, &vector_tail_1};
-  const auto vector_head_2 = Point{4, 0};
-  const auto vector_tail_2 = Point{5, 0};
-  const auto vector_2 = TwoDimensionalVector{&vector_head_2, &vector_tail_2};
+  const auto vector_1 = TwoDimensionalVector{{0, 0}, {4, 0}};
+  const auto vector_2 = TwoDimensionalVector{{4, 0}, {5, 0}};
 
   ASSERT_THROW({ const auto triangle = Triangle(&vector_1, &vector_2); },
                Triangle::ParallelSideException);
