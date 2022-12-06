1c7bb2404f50e7b49f73ad0f11c73416fa94608f
Lai-YT <381xvmvbib@gmail.com>
2022-12-06 13:35:02 +0000

    refactor: remove unnecessary heap allocation

--- src/builder/shape_parser.h
+++ src/builder/shape_parser.h
@@ -17,12 +17,6 @@ class ShapeParser {
  public:
   ShapeParser(const std::string& input) : scanner_{input} {}
 
-  ~ShapeParser() {
-    for (auto&& p : points_to_delete_) {
-      delete p;
-    }
-  }
-
   void parse() {
     while (!scanner_.isDone()) {
       auto token = std::string{scanner_.next()};
@@ -49,66 +43,47 @@ class ShapeParser {
   Scanner scanner_;
   ShapeBuilder builder_{};
 
-  /* This is to extend the life time of the points,
-   * but they are still deleted with the parser. */
-  std::vector<const Point*> points_to_delete_{};
-
   void ParseCircle_() {
-    auto* center = new Point{scanner_.nextDouble(), scanner_.nextDouble()};
-    auto* on_circle = new Point{scanner_.nextDouble(), scanner_.nextDouble()};
-    DeleteLater_(center);
-    DeleteLater_(on_circle);
+    auto center = Point{scanner_.nextDouble(), scanner_.nextDouble()};
+    auto on_circle = Point{scanner_.nextDouble(), scanner_.nextDouble()};
     builder_.buildCircle(center, on_circle);
     SkipTripleCloseParenthesis_();
   }
 
   void ParseTriangle_() {
-    std::array<const Point*, 3> vertices =
+    std::array<Point, 3> vertices =
         ExtractThreeUnDuplicatedVerticesFromNextFourPoints_();
     builder_.buildTriangle(vertices.at(0), vertices.at(1), vertices.at(2));
     SkipTripleCloseParenthesis_();
   }
 
   void ParseRectangle_() {
-    std::array<const Point*, 3> vertices =
+    std::array<Point, 3> vertices =
         ExtractThreeUnDuplicatedVerticesFromNextFourPoints_();
     builder_.buildRectangle(vertices.at(0), vertices.at(1), vertices.at(2));
     SkipTripleCloseParenthesis_();
   }
 
-  /* The 3 un-duplicated vertices will be deleted later. */
-  std::array<const Point*, 3>
-  ExtractThreeUnDuplicatedVerticesFromNextFourPoints_() {
-    auto points_info_less_than = [](const Point& p1, const Point& p2) -> bool {
-      return p1.info() < p2.info();
-    };
+  std::array<Point, 3> ExtractThreeUnDuplicatedVerticesFromNextFourPoints_() {
     /* Since two of them are common points, put them into a set to eliminate the
      * common point. */
-    auto vertices = std::set<Point, decltype(points_info_less_than)>{
-        {
-            Point{scanner_.nextDouble(), scanner_.nextDouble()},
-            Point{scanner_.nextDouble(), scanner_.nextDouble()},
-            Point{scanner_.nextDouble(), scanner_.nextDouble()},
-            Point{scanner_.nextDouble(), scanner_.nextDouble()},
-        },
-        points_info_less_than};
+    auto vertices = std::set<Point>{{
+        {scanner_.nextDouble(), scanner_.nextDouble()},
+        {scanner_.nextDouble(), scanner_.nextDouble()},
+        {scanner_.nextDouble(), scanner_.nextDouble()},
+        {scanner_.nextDouble(), scanner_.nextDouble()},
+    }};
     assert(vertices.size() == 3);
 
-    /* Allocate them on the heap so can extend their life-time. */
-    auto pointers_of_vertices = std::array<const Point*, 3>{};
+    auto DUMMY_POINT = Point{0, 0}; /* Point has no default ctor */
+    auto pointers_of_vertices =
+        std::array<Point, 3>{DUMMY_POINT, DUMMY_POINT, DUMMY_POINT};
     int i = 0;
     for (auto&& p : vertices) {
-      pointers_of_vertices.at(i) = new Point{p};
+      pointers_of_vertices.at(i) = Point{p};
       ++i;
     }
-    for (const Point* p : pointers_of_vertices) {
-      DeleteLater_(p);
-    }
     return pointers_of_vertices;
-  }
-
-  void DeleteLater_(const Point* const to_delete) {
-    points_to_delete_.push_back(to_delete);
   }
 
   void SkipTripleCloseParenthesis_() {
