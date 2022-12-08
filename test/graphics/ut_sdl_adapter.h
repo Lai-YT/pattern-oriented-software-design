fd1134cf290a6fb373a8875e1bae7969a6207300
Lai-YT <381xvmvbib@gmail.com>
2022-12-08 10:49:46 +0000

    refactor: test fixture

--- test/graphics/ut_sdl_adapter.h
+++ test/graphics/ut_sdl_adapter.h
@@ -7,70 +7,62 @@
 #include "../../src/triangle.h"
 #include "mock_sdl_renderer.h"
 
-TEST(SDLAdapterTest, InitShouldBeCalled) {
-  auto mockSDLRenderer = MockSDLRenderer{};
+class SDLAdapterTest : public testing::Test {
+ protected:
+  const double DELTA = 0.001;
 
-  auto canvas = SDLAdapter{1024, 768, &mockSDLRenderer};
+  MockSDLRenderer mock_sdl_renderer_{};
+  SDLAdapter canvas_{1024, 768, &mock_sdl_renderer_};
+};
 
-  ASSERT_TRUE(mockSDLRenderer.isInitCalled());
+TEST_F(SDLAdapterTest, InitShouldBeCalled) {
+  ASSERT_TRUE(mock_sdl_renderer_.isInitCalled());
 }
 
-TEST(SDLAdapterTest, InitShouldBeCalledWithCorrectWidthAndHeight) {
-  const int width = 1024;
-  const int height = 768;
-  auto mockSDLRenderer = MockSDLRenderer{};
-
-  auto canvas = SDLAdapter{width, height, &mockSDLRenderer};
-
-  ASSERT_EQ(width, mockSDLRenderer.initWidth());
-  ASSERT_EQ(height, mockSDLRenderer.initHeight());
+TEST_F(SDLAdapterTest, InitShouldBeCalledWithCorrectWidthAndHeight) {
+  ASSERT_EQ(1024, mock_sdl_renderer_.initWidth());
+  ASSERT_EQ(768, mock_sdl_renderer_.initHeight());
 }
 
-TEST(SDLAdapterTest, InitWithNonPositiveWidthShouldThrowException) {
+TEST_F(SDLAdapterTest, InitWithNonPositiveWidthShouldThrowException) {
   const int non_positive_width = 0;
-  auto mockSDLRenderer = MockSDLRenderer{};
 
-  ASSERT_THROW(SDLAdapter(non_positive_width, 768, &mockSDLRenderer),
+  ASSERT_THROW(SDLAdapter(non_positive_width, 768, &mock_sdl_renderer_),
                SDLAdapter::NonPositiveLengthOfSideException);
 }
 
-TEST(SDLAdapterTest, InitWithNonPositiveHeightShouldThrowException) {
+TEST_F(SDLAdapterTest, InitWithNonPositiveHeightShouldThrowException) {
   const int non_positive_height = 0;
-  auto mockSDLRenderer = MockSDLRenderer{};
 
-  ASSERT_THROW(SDLAdapter(1024, non_positive_height, &mockSDLRenderer),
+  ASSERT_THROW(SDLAdapter(1024, non_positive_height, &mock_sdl_renderer_),
                SDLAdapter::NonPositiveLengthOfSideException);
 }
-TEST(SDLAdapterTest,
-     DrawCircleShouldCallRenderDrawCircleWithCorrectCentreAndRadius) {
+TEST_F(SDLAdapterTest,
+       DrawCircleShouldCallRenderDrawCircleWithCorrectCentreAndRadius) {
   const auto circle = Circle{{{1, 2}, {4, 6}}};
-  auto mockSDLRenderer = MockSDLRenderer{};
-  auto canvas = SDLAdapter{1024, 768, &mockSDLRenderer};
 
-  canvas.drawCircle(&circle);
+  canvas_.drawCircle(&circle);
 
-  const double* args = mockSDLRenderer.renderDrawCircleCalledArgs();
-  EXPECT_NEAR(1, args[0], 0.001);
-  EXPECT_NEAR(2, args[1], 0.001);
-  EXPECT_NEAR(5, args[2], 0.001);
+  const double* args = mock_sdl_renderer_.renderDrawCircleCalledArgs();
+  EXPECT_NEAR(1, args[0], DELTA);
+  EXPECT_NEAR(2, args[1], DELTA);
+  EXPECT_NEAR(5, args[2], DELTA);
 }
 
-TEST(SDLAdapterTest, DrawTriangleShouldCallRenderDrawLinesWithCorrectPoints) {
+TEST_F(SDLAdapterTest, DrawTriangleShouldCallRenderDrawLinesWithCorrectPoints) {
   const auto triangle = Triangle{{{0, 0}, {3, 0}}, {{3, 4}, {3, 0}}};
   const int expected_size = 6;
   const double expected_points[expected_size] = {0, 0, 3, 0, 3, 4};
-  auto mockSDLRenderer = MockSDLRenderer{};
-  auto canvas = SDLAdapter{1024, 768, &mockSDLRenderer};
 
-  canvas.drawTriangle(&triangle);
+  canvas_.drawTriangle(&triangle);
 
   /* XXX: the mocking class takes the ownership, while the real class doesn't.
    */
-  const int size = mockSDLRenderer.renderDrawLinesCalledSize();
+  const int size = mock_sdl_renderer_.renderDrawLinesCalledSize();
   ASSERT_EQ(expected_size, size);
-  const double* points = mockSDLRenderer.renderDrawLinesCalledPoints();
+  const double* points = mock_sdl_renderer_.renderDrawLinesCalledPoints();
   for (int i = 0; i < size; i++) {
-    ASSERT_NEAR(expected_points[i], points[i], 0.001);
+    ASSERT_NEAR(expected_points[i], points[i], DELTA);
   }
   delete[] points;
 }
