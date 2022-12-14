a2b0aefec5fc7ed2b6a854b1de03c25da452fce8
Lai-YT <381xvmvbib@gmail.com>
2022-12-14 14:43:59 +0000

    feat: execute should call drop with the current position

--- test/graphics/drag_and_drop/command/ut_drop_command.h
+++ test/graphics/drag_and_drop/command/ut_drop_command.h
@@ -2,18 +2,25 @@
 
 #include "../../../../src/graphics/drag_and_drop/command/command_history.h"
 #include "../../../../src/graphics/drag_and_drop/command/drop_command.h"
-#include "../../../../src/graphics/drag_and_drop/command/grab_command.h"
 #include "../../../../src/graphics/drag_and_drop/mouse_position.h"
 #include "../mock_drag_and_drop.h"
 
-TEST(DropCommandTest, ExecuteShouldCallDropMethodWithCorrectMousePosition) {
-  MockDragAndDrop mock_drag_and_drop;
-  CommandHistory history;
-  MousePosition::getInstance()->setPos(85.23, 232.7);
-  DropCommand dropCommand(&mock_drag_and_drop, &history);
+class DropCommandTest : public testing::Test {
+ protected:
+  const double DELTA = 0.001;
 
-  dropCommand.execute();
+  MockDragAndDrop mock_drag_and_drop_{};
+  CommandHistory history_{};
+  DropCommand drop_command_{&mock_drag_and_drop_, &history_};
+};
 
-  ASSERT_NEAR(85.23, mock_drag_and_drop.getDropX(), 0.001);
-  ASSERT_NEAR(232.7, mock_drag_and_drop.getDropY(), 0.001);
+TEST_F(DropCommandTest, ExecuteShouldCallDropWithCurrentMousePosition) {
+  const double curr_x = 85.23;
+  const double curr_y = 232.7;
+  MousePosition::getInstance()->setPos(curr_x, curr_y);
+
+  drop_command_.execute();
+
+  ASSERT_NEAR(curr_x, mock_drag_and_drop_.getDropX(), DELTA);
+  ASSERT_NEAR(curr_y, mock_drag_and_drop_.getDropY(), DELTA);
 }
