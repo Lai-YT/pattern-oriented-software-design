ec6cc666411e232bd768c2dd4c93eb8730fb07be
Lai-YT <381xvmvbib@gmail.com>
2022-12-14 22:06:05 +0800

    Feat `Execute` & `Undo` of `GrabCommand` (#6)
    * feat: execute should be called with the current mouse position
    
    * feat: undoable
    
    * refactor: test fixture
    
    * refactor: extract method
    
    * feat: get x & y

--- src/graphics/drag_and_drop/command/grab_command.h
+++ src/graphics/drag_and_drop/command/grab_command.h
@@ -2,21 +2,56 @@
 #define SRC_GRAPHICS_DRAG_AND_DROP_COMMAND_GRAB_COMMAND_H_
 
 #include "../drag_and_drop.h"
+#include "../mouse_position.h"
 #include "command.h"
 #include "command_history.h"
 
 class GrabCommand : public Command {
  public:
-  GrabCommand(DragAndDrop* drag_and_drop, CommandHistory* command_history) {}
-  GrabCommand(const GrabCommand& grab_command) {}
+  GrabCommand(DragAndDrop* drag_and_drop, CommandHistory* command_history)
+      : drag_and_drop_{drag_and_drop}, command_history_{command_history} {}
 
-  void execute() override {}
+  GrabCommand(const GrabCommand& grab_command) =
+      default; /* shallow copy is allowed since the pointers are shared */
 
-  void undo() override {}
+  void execute() override {
+    /* so undo-able */
+    was_executed_ = true;
+    RecordPosition_();
 
-  double getX() const {}
+    auto* curr_mouse_pos = MousePosition::getInstance();
+    drag_and_drop_->grab(curr_mouse_pos->getX(), curr_mouse_pos->getY());
+  }
 
-  double getY() const {}
+  void undo() override {
+    if (was_executed_) {
+      drag_and_drop_->move(prev_x_, prev_y_);
+    }
+  }
+
+  double getX() const {
+    return prev_x_;
+  }
+
+  double getY() const {
+    return prev_y_;
+  }
+
+ private:
+  DragAndDrop* drag_and_drop_;
+  CommandHistory* command_history_;
+
+  /* states for undo */
+  bool was_executed_ = false;
+  /* previous x and y are meaningless unless was executed */
+  double prev_x_ = 0;
+  double prev_y_ = 0;
+
+  void RecordPosition_() {
+    auto* curr_mouse_pos = MousePosition::getInstance();
+    prev_x_ = curr_mouse_pos->getX();
+    prev_y_ = curr_mouse_pos->getY();
+  }
 };
 
 #endif /* end of include guard: \
---
+++ test/graphics/drag_and_drop/command/ut_grab_command.h
@@ -0,0 +1,66 @@
+#include <gtest/gtest.h>
+
+#include "../../../../src/graphics/drag_and_drop/command/command_history.h"
+#include "../../../../src/graphics/drag_and_drop/command/grab_command.h"
+#include "../../../../src/graphics/drag_and_drop/mouse_position.h"
+#include "../mock_drag_and_drop.h"
+
+class GrabCommandTest : public testing::Test {
+ protected:
+  const double DELTA = 0.001;
+
+  MockDragAndDrop mock_drag_and_drop_{};
+  CommandHistory history_{};
+  GrabCommand grab_command_{&mock_drag_and_drop_, &history_};
+};
+
+TEST_F(GrabCommandTest, ExecuteShouldCallGrabWithCurrentMousePosition) {
+  const double curr_x = 85.23;
+  const double curr_y = 232.7;
+  MousePosition::getInstance()->setPos(curr_x, curr_y);
+
+  grab_command_.execute();
+
+  ASSERT_NEAR(curr_x, mock_drag_and_drop_.getGrabX(), DELTA);
+  ASSERT_NEAR(curr_y, mock_drag_and_drop_.getGrabY(), DELTA);
+}
+
+TEST_F(GrabCommandTest, UndoWithoutPreCalledExecuteShouldHaveNoEffect) {
+  const double prev_x = 10;
+  const double prev_y = 20;
+  const double NOT_CALLED = 0; /* initial value used in MockDragAndDrop */
+  MousePosition::getInstance()->setPos(prev_x, prev_y);
+
+  grab_command_.undo();
+
+  auto* curr_pos = MousePosition::getInstance();
+  ASSERT_NEAR(NOT_CALLED, mock_drag_and_drop_.getMoveX(), DELTA);
+  ASSERT_NEAR(NOT_CALLED, mock_drag_and_drop_.getMoveY(), DELTA);
+}
+
+TEST_F(GrabCommandTest, UndoShouldCallDropWithPreviousMousePosition) {
+  const double prev_x = 10;
+  const double prev_y = 20;
+  MousePosition::getInstance()->setPos(prev_x, prev_y);
+  /* to undo, you have to execute first */
+  grab_command_.execute();
+
+  grab_command_.undo();
+
+  ASSERT_NEAR(prev_x, mock_drag_and_drop_.getMoveX(), DELTA);
+  ASSERT_NEAR(prev_y, mock_drag_and_drop_.getMoveY(), DELTA);
+}
+
+TEST_F(GrabCommandTest,
+       GetXAndYShouldReturnThePositionWhereTheCommandWasExecuted) {
+  const double executed_x = 10;
+  const double executed_y = 20;
+  MousePosition::getInstance()->setPos(executed_x, executed_y);
+  grab_command_.execute();
+
+  const double got_x = grab_command_.getX();
+  const double got_y = grab_command_.getY();
+
+  ASSERT_NEAR(executed_x, got_x, DELTA);
+  ASSERT_NEAR(executed_y, got_y, DELTA);
+}
