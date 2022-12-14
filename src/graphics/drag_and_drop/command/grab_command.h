fc6f851f13cb265cb2070645dfbb66c15c39ac50
Lai-YT <381xvmvbib@gmail.com>
2022-12-14 15:07:48 +0000

    refactor: rename variable

--- src/graphics/drag_and_drop/command/grab_command.h
+++ src/graphics/drag_and_drop/command/grab_command.h
@@ -25,16 +25,16 @@ class GrabCommand : public Command {
 
   void undo() override {
     if (was_executed_) {
-      drag_and_drop_->move(prev_x_, prev_y_);
+      drag_and_drop_->move(grabbed_x_, grabbed_y_);
     }
   }
 
   double getX() const {
-    return prev_x_;
+    return grabbed_x_;
   }
 
   double getY() const {
-    return prev_y_;
+    return grabbed_y_;
   }
 
  private:
@@ -43,14 +43,14 @@ class GrabCommand : public Command {
 
   /* states for undo */
   bool was_executed_ = false;
-  /* previous x and y are meaningless unless was executed */
-  double prev_x_ = 0;
-  double prev_y_ = 0;
+  /* grabbed x and y are meaningless unless was executed */
+  double grabbed_x_ = 0;
+  double grabbed_y_ = 0;
 
   void RecordPosition_() {
     auto* curr_mouse_pos = MousePosition::getInstance();
-    prev_x_ = curr_mouse_pos->getX();
-    prev_y_ = curr_mouse_pos->getY();
+    grabbed_x_ = curr_mouse_pos->getX();
+    grabbed_y_ = curr_mouse_pos->getY();
   }
 };
 
