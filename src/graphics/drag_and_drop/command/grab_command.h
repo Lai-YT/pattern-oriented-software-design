648ccd4dc40505dd24242757528a9f53a4fbb494
Lai-YT <381xvmvbib@gmail.com>
2022-12-14 13:44:26 +0000

    feat: undoablewq

--- src/graphics/drag_and_drop/command/grab_command.h
+++ src/graphics/drag_and_drop/command/grab_command.h
@@ -17,9 +17,16 @@ class GrabCommand : public Command {
   void execute() override {
     auto* curr_mouse_pos = MousePosition::getInstance();
     drag_and_drop_->grab(curr_mouse_pos->getX(), curr_mouse_pos->getY());
+    was_executed_ = true;
+    prev_x_ = curr_mouse_pos->getX();
+    prev_y_ = curr_mouse_pos->getY();
   }
 
-  void undo() override {}
+  void undo() override {
+    if (was_executed_) {
+      drag_and_drop_->move(prev_x_, prev_y_);
+    }
+  }
 
   double getX() const {}
 
@@ -28,6 +35,10 @@ class GrabCommand : public Command {
  private:
   DragAndDrop* drag_and_drop_;
   CommandHistory* command_history_;
+  bool was_executed_ = false;
+  /* previous x and y are meaningless unless was executed */
+  double prev_x_ = 0;
+  double prev_y_ = 0;
 };
 
 #endif /* end of include guard: \
--- test/graphics/drag_and_drop/command/ut_grab_command.h
+++ test/graphics/drag_and_drop/command/ut_grab_command.h
@@ -5,7 +5,7 @@
 #include "../../../../src/graphics/drag_and_drop/mouse_position.h"
 #include "../mock_drag_and_drop.h"
 
-TEST(GrabCommandTest, ExecuteShouldGrabMethodWithCorrectMousePosition) {
+TEST(GrabCommandTest, ExecuteShouldCallGrabWithCorrectMousePosition) {
   MockDragAndDrop mock_drag_and_drop{};
   CommandHistory history{};
   const double grab_x = 85.23;
@@ -17,4 +17,36 @@ TEST(GrabCommandTest, ExecuteShouldGrabMethodWithCorrectMousePosition) {
 
   ASSERT_NEAR(grab_x, mock_drag_and_drop.getGrabX(), 0.001);
   ASSERT_NEAR(grab_y, mock_drag_and_drop.getGrabY(), 0.001);
+}
+
+TEST(GrabCommandTest, UndoWithoutPreCalledExecuteShouldHaveNoEffect) {
+  MockDragAndDrop mock_drag_and_drop{};
+  CommandHistory history{};
+  const double prev_x = 10;
+  const double prev_y = 20;
+  const double NOT_CALLED = 0; /* initial value used in mock_drag_and_drop */
+  MousePosition::getInstance()->setPos(prev_x, prev_y);
+  GrabCommand grab_command{&mock_drag_and_drop, &history};
+
+  grab_command.undo();
+
+  auto* curr_pos = MousePosition::getInstance();
+  ASSERT_NEAR(NOT_CALLED, mock_drag_and_drop.getMoveX(), 0.001);
+  ASSERT_NEAR(NOT_CALLED, mock_drag_and_drop.getMoveY(), 0.001);
+}
+
+TEST(GrabCommandTest, UndoShouldCallDropWithPreviousMousePosition) {
+  MockDragAndDrop mock_drag_and_drop{};
+  CommandHistory history{};
+  const double prev_x = 10;
+  const double prev_y = 20;
+  MousePosition::getInstance()->setPos(prev_x, prev_y);
+  GrabCommand grab_command{&mock_drag_and_drop, &history};
+  /* to undo, you have to execute first */
+  grab_command.execute();
+
+  grab_command.undo();
+
+  ASSERT_NEAR(prev_x, mock_drag_and_drop.getMoveX(), 0.001);
+  ASSERT_NEAR(prev_y, mock_drag_and_drop.getMoveY(), 0.001);
 }
