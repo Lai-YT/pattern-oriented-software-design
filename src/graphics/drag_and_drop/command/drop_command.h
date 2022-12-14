d69f967715bcb29703a896fe8dcb0d4fce834e3b
Lai-YT <381xvmvbib@gmail.com>
2022-12-14 14:58:37 +0000

    feat: undoable

--- src/graphics/drag_and_drop/command/drop_command.h
+++ src/graphics/drag_and_drop/command/drop_command.h
@@ -15,11 +15,19 @@ class DropCommand : public Command {
       default; /* shallow copy is allowed since the pointers are shared */
 
   void execute() override {
+    /* so undo-able */
+    was_executed_ = true;
+    RecordPosition_();
+
     auto* curr_mouse_pos = MousePosition::getInstance();
     drag_and_drop_->drop(curr_mouse_pos->getX(), curr_mouse_pos->getY());
   }
 
-  void undo() override {}
+  void undo() override {
+    if (was_executed_) {
+      drag_and_drop_->grab(dropped_x_, dropped_y_);
+    }
+  }
 
   double getX() const {}
 
@@ -28,6 +36,18 @@ class DropCommand : public Command {
  private:
   DragAndDrop* drag_and_drop_;
   CommandHistory* command_history_;
+
+  /* states for undo */
+  bool was_executed_ = false;
+  /* dropped x and y are meaningless unless was executed */
+  double dropped_x_ = 0;
+  double dropped_y_ = 0;
+
+  void RecordPosition_() {
+    auto* curr_mouse_pos = MousePosition::getInstance();
+    dropped_x_ = curr_mouse_pos->getX();
+    dropped_y_ = curr_mouse_pos->getY();
+  }
 };
 
 #endif /* end of include guard: \
--- test/graphics/drag_and_drop/command/ut_drop_command.h
+++ test/graphics/drag_and_drop/command/ut_drop_command.h
@@ -24,3 +24,29 @@ TEST_F(DropCommandTest, ExecuteShouldCallDropWithCurrentMousePosition) {
   ASSERT_NEAR(curr_x, mock_drag_and_drop_.getDropX(), DELTA);
   ASSERT_NEAR(curr_y, mock_drag_and_drop_.getDropY(), DELTA);
 }
+
+TEST_F(DropCommandTest, UndoWithoutPreCalledExecuteShouldHaveNoEffect) {
+  const double prev_x = 10;
+  const double prev_y = 20;
+  const double NOT_CALLED = 0; /* initial value used in MockDragAndDrop */
+  MousePosition::getInstance()->setPos(prev_x, prev_y);
+
+  drop_command_.undo();
+
+  auto* curr_pos = MousePosition::getInstance();
+  ASSERT_NEAR(NOT_CALLED, mock_drag_and_drop_.getDropX(), DELTA);
+  ASSERT_NEAR(NOT_CALLED, mock_drag_and_drop_.getDropY(), DELTA);
+}
+
+TEST_F(DropCommandTest, UndoShouldCallGrabWithWhereWasDropped) {
+  const double dropped_x = 10;
+  const double dropped_y = 20;
+  MousePosition::getInstance()->setPos(dropped_x, dropped_y);
+  /* to undo, you have to execute first */
+  drop_command_.execute();
+
+  drop_command_.undo();
+
+  ASSERT_NEAR(dropped_x, mock_drag_and_drop_.getGrabX(), DELTA);
+  ASSERT_NEAR(dropped_y, mock_drag_and_drop_.getGrabY(), DELTA);
+}
