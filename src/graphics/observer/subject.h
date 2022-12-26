2a42b1e88e889884bdc923feb2355153a54b4bbc
Lai-YT <381xvmvbib@gmail.com>
2022-12-26 13:56:30 +0800

    feat: attaching observers

--- src/graphics/observer/subject.h
+++ src/graphics/observer/subject.h
@@ -4,11 +4,13 @@
 #include <stdexcept>
 #include <vector>
 
-class Observer;
+#include "observer.h"
 
 class Subject {
  public:
-  void attach(Observer* observer) {}
+  void attach(Observer* observer) {
+    observers_.push_back(observer);
+  }
 
   /** @throw ObserverNotFoundError */
   void detach(Observer* observer) {}
@@ -16,13 +18,18 @@ class Subject {
   void notify() {}
 
   /** @brief Returns the observers for testability. */
-  std::vector<Observer*> getObservers() const {}
+  std::vector<Observer*> getObservers() const {
+    return observers_;
+  }
 
   virtual ~Subject() = default; /* subject does not own the observers */
 
   class ObserverNotFoundError : public std::runtime_error {
     using std::runtime_error::runtime_error;
   };
+
+ private:
+  std::vector<Observer*> observers_{};
 };
 
 #endif /* end of include guard: SRC_GRAPHICS_OBSERVER_SUBJECT_H_ */
