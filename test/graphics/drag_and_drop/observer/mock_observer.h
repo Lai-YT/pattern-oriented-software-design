a5657551d7cac0d7aeec8f1c7837ba77f3743315
Lai-YT <381xvmvbib@gmail.com>
2022-12-26 13:45:31 +0800

    test: add mocking utils

---
+++ test/graphics/drag_and_drop/observer/mock_observer.h
@@ -0,0 +1,24 @@
+/*
+ * This class is provided by the TAs.
+ */
+
+#pragma once
+
+#include "../../../src/graphics/observer/observer.h"
+
+class MockObserver : public Observer
+{
+private:
+    bool _isUpdated = false;
+
+public:
+    void update() override
+    {
+        _isUpdated = true;
+    }
+
+    bool isUpdated()
+    {
+        return _isUpdated;
+    }
+};
